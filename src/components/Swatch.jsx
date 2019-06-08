fe2072d67921c03bc0de6a0754d73e71950e62b0
Andrey Okonetchnikov <andrey@okonet.ru>
2019-06-08 17:11:09 +0200

    fix: Remove hover styles from Swatch

--- src/components/Swatch.jsx
+++ src/components/Swatch.jsx
@@ -1,32 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "@emotion/styled";
 import copy from "clipboard-copy";
-import { css } from "theme-ui";
-
-const SwatchBase = styled("div")`
-	&:hover,
-	&:active,
-	&:focus {
-		outline: 0;
-		box-shadow: inset 0 0 0 2px rgba(0, 0, 0, 0.7);
-		cursor: pointer;
-	}
-`;
 
 function Swatch(props) {
-	console.log(props);
 	return (
-		<SwatchBase
-			{...props}
+		<div
+			aria-roledescription="Copy to clipboard"
 			onClick={() => copy(props.token)}
-			css={css({
-				"&:hover, &:active, &:focus": {
-					bg: "muted"
-				}
-				// fontFamily: "body",
-				// fontSize: "md"
-			})}
+			css={{
+				cursor: "pointer"
+			}}
+			{...props}
 		/>
 	);
 }
